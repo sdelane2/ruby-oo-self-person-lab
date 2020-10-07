ffef1705da1dd6ba9ecd01203f42fa37ef46fdc0
sdelane2 <seanmartindelaney@gmail.com>
2020-10-07 11:13:25 -0400

    oo self person lab

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,25 @@
 # your code goes here
+class Person
+    attr_reader :name
+    attr_accessor :bank_account, :happiness, :hygiene
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+    def clean?
+        if @hygiene > 7
+            true 
+        else
+            false
+        end
+    end
+    def happy?
+        if @happiness > 7
+            true
+        else
+            false
+        end
+    end
+end
